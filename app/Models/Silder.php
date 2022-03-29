cd8e6a6860837313c59ba5d91236bc951d2d6eb4
Apurbo Kumar Anup <apurboka@gmail.com>
2022-03-29 18:06:02 +0600

    Silder.php

--- app/Models/Silder.php
+++ app/Models/Silder.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Silder extends Model
 {
     use HasFactory;
+    const ACTIVE_STATUS = 'active';
+    const INACTIVE_STATUS = 'inactive';
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'image',
+    ];
 }
