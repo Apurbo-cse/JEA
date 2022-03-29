65a97bfa0764917bc8f40102a31b700493803b9f
Apurbo Kumar Anup <apurboka@gmail.com>
2022-03-29 17:03:02 +0600

    JEA

---
+++ resources/views/backend/category/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('backend.layouts.master')
+
+@section('main-content')
+
+<div class="card">
+    <h5 class="card-header">Edit Category</h5>
+    <div class="card-body">
+      <form method="post" >
+        <div class="form-group">
+          <label for="inputTitle" class="col-form-label">Title <span class="text-danger">*</span></label>
+          <input id="inputTitle" type="text" name="title" placeholder="Enter title" class="form-control">
+
+        </div>
+
+        <div class="form-group">
+          <label for="summary" class="col-form-label">Summary</label>
+          <textarea class="form-control" id="summary" name="summary">Lorem ipsum dolor sit amet consectetur adipisicing elit.</textarea>
+
+        </div>
+
+        <div class="form-group">
+          <label for="is_parent">Is Parent</label><br>
+          <input type="checkbox" name='is_parent' id='is_parent'> Yes
+        </div>
+        {{-- {{$parent_cats}} --}}
+        {{-- {{$category}} --}}
+
+      <div class="form-group" id='parent_cat_div'>
+          <label for="parent_id">Parent Category</label>
+          <select name="parent_id" class="form-control">
+              <option value="">--Select any category--</option>
+              <option value="">dfgdfgdf</option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="inputPhoto" class="col-form-label">Photo</label>
+          <div class="input-group">
+              <span class="input-group-btn">
+                  <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                  <i class="fa fa-picture-o"></i> Choose
+                  </a>
+              </span>
+          <input id="thumbnail" class="form-control" type="text" name="photo" >
+        </div>
+
+        <div class="form-group">
+          <label for="status" class="col-form-label">Status <span class="text-danger">*</span></label>
+          <select name="status" class="form-control">
+              <option value="active" >Active</option>
+              <option value="inactive" >Inactive</option>
+          </select>
+        </div>
+        <div class="form-group mb-3">
+           <button class="btn btn-success" type="submit">Update</button>
+        </div>
+      </form>
+    </div>
+</div>
+
+@endsection
---
+++ routes/admin.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/dashboard', function () {
+    return view('backend.index');
+})->middleware(['auth'])->name('admin.dashboard');
+
+
+Route::group(['prefix' => 'dashboard/', 'as' => 'admin.', 'middleware' => ['auth']], function () {
+
+
+
+    Route::group(['prefix' => 'category/', 'as' => 'category.'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('index');
+        Route::get('create', [CategoryController::class, 'create'])->name('create');
+        Route::get('edit', [CategoryController::class, 'edit'])->name('edit');
+    });
+
+});
+
+
+
+
+
+
+
+
